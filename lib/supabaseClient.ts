948836ed34b74818c6da6b41f61cdc363b20d1f7
immanuelniik-dev <immanuelniik@gmail.com>
2026-01-12 18:51:29 +0000

    Hardcode keys to fix login

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// WE ARE PASTING THE KEYS DIRECTLY HERE TO FORCE IT TO WORK
+const supabaseUrl = "https://lyxngvtdreuwsgdmgeoi.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5eG5ndnRkcmV1d3NnZG1nZW9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNDQ1NTYsImV4cCI6MjA4MzcyMDU1Nn0.LL9PBOhVD7HSc89Eyel72p49PN6RjJ__ByiyQP8UIvQ"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
